13b98b1fa75159f185c44773b13005b8e469596c
LongMeow <doanthelong061207@gmail.com>
2026-05-06 17:28:00 +0700

    add build method

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ from textnode import TextNode, TextType
 from blocknode import markdown_to_html
 import os, shutil
 from pathlib import Path
+import sys 
 
 def main(): 
-  if os.path.exists("public"): 
-    shutil.rmtree("public") 
-  shutil.copytree("static", "public")
+  basepath = sys.argv[0] if len(sys.argv) > 0 else "/"
+  if os.path.exists("docs"): 
+    shutil.rmtree("docs") 
+  shutil.copytree("static", "docs")
   base_content_dir = Path("content")
   for file_path in base_content_dir.rglob("index.md"):
-    generate_page(file_path, "template.html", Path(str(file_path).replace("content", "public").replace(".md", ".html")))
+    generate_page(file_path, "template.html", Path(str(file_path).replace("content", "docs").replace(".md", ".html")), basepath)
   
 def extract_title(markdown): 
   lines = markdown.split("\n") 
@@ -17,7 +19,7 @@ def extract_title(markdown):
     if line[:2] == "# ": 
       return line[2:].strip()
     
-def generate_page(from_path, template_path, dest_path): 
+def generate_page(from_path, template_path, dest_path, basepath): 
   print(f"Generating page from {from_path} to {dest_path} using {template_path}")
   with open(from_path, 'r') as file: 
     content = file.read() 
@@ -25,7 +27,8 @@ def generate_page(from_path, template_path, dest_path):
   title = extract_title(content) 
   with open(template_path, 'r') as file: 
     template_content = file.read() 
-  full_template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content) 
+  full_template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+  full_template_content = full_template_content.replace("href=\"", f"href=\"{basepath}\"").replace("src=\"", f"src=\"{basepath}\"")
   dest_path.parent.mkdir(parents=True, exist_ok=True)
   dest_path.write_text(full_template_content)
   
